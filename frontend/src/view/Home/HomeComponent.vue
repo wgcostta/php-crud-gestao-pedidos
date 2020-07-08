1a10d4b74d873aaa1db310a8091489b26aa1536a
Wagner <wg.o.costa@gmail.com>
2020-07-08 20:48:16 -0300

    Ajustando rotas e chamando o Axios

--- frontend/src/view/Home/HomeComponent.vue
+++ frontend/src/view/Home/HomeComponent.vue
@@ -6,7 +6,31 @@
 
 <script>
 import DashBoardComponent from "./DashBoardComponent";
+const axios = require("axios");
+
 export default {
+  name: "HomeComponent",
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (response.status == 200) {
+        this.uses = response.data;
+      } else {
+        console.error("Ocorreu um erro na API.");
+      }
+    }
+  },
+
   components: {
     DashBoardComponent
   }
